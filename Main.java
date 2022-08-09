8372aabc037af1c142cc4f7e2b6107672961d6e8
Atheist777 <102061405+Atheist777@users.noreply.github.com>
2022-08-09 17:52:46 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,55 +2,48 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-
-
 public class Main {
 
+    public static void main(String[] args) throws IOException {
 
-
-    public static void main(String[] args) {
-        String gamesFile = "C:\\Games\\";
         StringBuilder stringBuilder = new StringBuilder();
-        
-        stringBuilder.append(createDir(gamesFile + "src"));
-        stringBuilder.append(createDir(gamesFile + "res"));
-        stringBuilder.append(createDir(gamesFile + "savegames"));
-        stringBuilder.append(createDir(gamesFile + "temp"));
-        stringBuilder.append(createDir(gamesFile + "src\\main"));
-        stringBuilder.append(createDir(gamesFile + "src\\test"));
-        stringBuilder.append(createFile(gamesFile + "src\\main\\Main.java"));
-        stringBuilder.append(createFile(gamesFile + "src\\main\\Utils.java"));
-        stringBuilder.append(createDir(gamesFile + "res\\drawables"));
-        stringBuilder.append(createDir(gamesFile + "res\\vectors"));
-        stringBuilder.append(createDir(gamesFile + "res\\icons"));
-        stringBuilder.append(createFile(gamesFile + "temp\\temp.txt"));
 
-        try (FileWriter writer = new FileWriter(gamesFile + "temp\\temp.txt", false)) {
-            writer.write(String.valueOf(stringBuilder));
-            writer.flush();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+        File[] dirs = {new File("Games/savegames"),
+                new File("Games/temp"),
+                new File("Games/src/main"),
+                new File("Games/src/test"),
+                new File("Games/res/drawables"),
+                new File("Games/res/vectors"),
+                new File("Games/res/icons"),};
+        File[] files = {new File("Games/src/main/Main.java"),
+                new File("Games/src/main/Utils.java"),
+                new File("Games/temp/temp.txt")};
+
+        for (int i = 0; i < dirs.length; i++) {
+            if (dirs[i].mkdirs()) {
+                stringBuilder.append("Создана директория: " + dirs[i].getName() + "\n");
+            }
+        }
+
+        for (int i = 0; i < files.length; i++) {
+            try {
+                if (files[i].createNewFile()) {
+                    stringBuilder.append("Создан файл: " + files[i].getName() + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+            try {
+                FileWriter filewriter = new FileWriter("C://Games/temp/temp.txt");
+                filewriter.write(String.valueOf(stringBuilder));
+                filewriter.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(stringBuilder);
+
         }
     }
 
-    public static String createDir(String dirLink) {
-        File dir = new File(dirLink);
-        if (dir.mkdir()) {
-            return dir.getName() + " - Каталог создан" + "\n";
-        } else return dir.getName() + " - Каталог создать не удалось" + "\n";
-    }
 
-    public static String createFile(String fileLink) {
-        File file = new File(fileLink);
-        try {
-            if (file.createNewFile())
-                return file.getName() + " - Файл создан" + "\n";
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        return file.getName() + " - Файл создать не удалось" + "\n";
-
-
-
-    }
-}
